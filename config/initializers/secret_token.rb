a4fd99a513ec219a568257218c5cdb6681eb83a1
dobabeswe <chololoy@gmail.com>
2012-12-28 07:37:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheveMakers2::Application.config.secret_token = '4b3ed5c1f1246c1b5ec9a20690c314baf4b1d3bc4fcc73e6cc42024d9d74c880d1e2c3ea8fb3112b8f908ee39ed8afbca99af22d3e29c0b641eceba624e7c072'
