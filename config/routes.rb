ba838ac2bc9937c1133286566bb4ace46dc29ad0
dobabeswe <chololoy@gmail.com>
2013-01-12 20:04:05 -0600

    Display Users on the Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 CheveMakers2::Application.routes.draw do
-  devise_for :users
-
+  authenticated :user do
+    root to: 'static_pages#home'
+  end
   root to: 'static_pages#home'
   
+  devise_for :users
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
