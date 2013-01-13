7de0023a9cc4a11ab1d6ddc1a06fc9df1a4e6e9b
dobabeswe <chololoy@gmail.com>
2013-01-13 10:10:57 -0600

    Add Users controller, show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ CheveMakers2::Application.routes.draw do
   root to: 'static_pages#home'
   
   devise_for :users
+  resources :users
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
